76a2211d184ef714b3f2147c6ad58a4142a2b46f
Cian O'Sullivan <117362356@umail.ucc.ie>
2018-02-08 12:19:27 +0000

    Adding functions to calculate vat and discount

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -5,16 +5,38 @@
     
      <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="../mystylesheet.css" type="text/css"/>
+        <link rel="stylesheet" href="business.css" type="text/css"/>
     </head>
     
     <body>
-        <h4>Please Enter Your Details</h4>
+        
+        <h1 class="header">Checkout</h1>
+        <h3>Please enter you payment details</h3>
         
         <form action="EBus3.php" METHOD="POST">
+            
+            <lable for="name">Name</lable>
+            <br/>
+            <input type="text" id="name" maxlength="30"/>
+            
+            <br/>
+            <br/>
+            
+            <lable for="email">Email</lable>
+            <br/>
+            <input type="text" id="email">
+            
+            <br/>
+            <br/>
         
             <lable for="user_pin">PIN</lable>
+           <br/>
             <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4"/>
         
+            <br/>
+            <br/>
+            
             <button type="submit" id="btnPurchase" disabled>Proceed with Purchase</button>
         </form>
         
--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -1,6 +1,6 @@
 /* global $ */
-//We must calculate VAT and discount ourself
 
+// This function calculates the subtotal amiount based on product selected
 function calcSub()
 {
     var argSubTotal; //Creates Variable
@@ -20,13 +20,15 @@ function calcSub()
         argSubTotal = 200;
     }//End if statement
     
-    else 
+    else  
     {
         argSubTotal = 400;
     }//End if statement
     
     display(argSubTotal) //Passing variable in the display function
+    calcDisVatTotal(argSubTotal)
 }
+
 
 function display(parm1) //parm1 equals value of argSubTotal
 {
@@ -45,4 +47,35 @@ function enablebtnProceed()
 function disablebtnProceed()
 {
     $('#btnProceed').prop('disabled', true);
+}
+
+// This function calculates the VAT and discount amounts
+function calcDisVatTotal(parmSubTotal)
+{
+    var argDiscount, subMinusDis, argVat, argTotal;
+    
+    argDiscount = parmSubTotal * 0.05
+    subMinusDis = parmSubTotal - argDiscount
+    argVat = subMinusDis * 0.1
+   argTotal = subMinusDis + argVat
+    
+    showDis(argDiscount)
+    showVat(argVat)
+    showTotal(argTotal)
+    
+}
+
+function showDis(parm3)
+{
+    document.getElementById("discount").value = parm3
+}
+
+function showVat(parm4)
+{
+    document.getElementById("vat").value = parm4
+}
+
+function showTotal(parm5)
+{
+    document.getElementById("total").value= parm5
 }
