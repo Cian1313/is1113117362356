ccc18ce4b51f1c19fbf48b91c1ec04af1e28aaf7
Cian O'Sullivan <117362356@umail.ucc.ie>
2018-02-27 21:09:44 +0000

    Checking if a line of code works

--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -3,7 +3,7 @@
 // functuon to validate the customers details
 function validateDetails()
 {
-    var name, email, addressline1, town, county, card, pin;
+    var name, email, addressline1, town, county, card, pin, colour;
     
     name = document.getElementById("name").value;
     email = document.getElementById("email").value;
@@ -12,13 +12,14 @@ function validateDetails()
     county = document.getElementById("county");
     card = document.getElementById("card");
     pin = document.getElementById("user_pin").value;
+    colour == rgb(255, 0, 0)
     
     if(name == "" || email == "" || addressline1 == "" || town == "" || county == "" || card == "" || pin == "")
     {
         window.alert("Please ensure all required field are filled(*)");
             if(name == "")
             {
-                document.getElementById("name").value = "Name is required"
+                document.getElementById("name").background == colour
             }
     }    
     else
